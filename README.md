f7be2609dd7b375c777b9148b57a1de83e0e7e39
Gateway47 <itsregalo047@gmail.com>
2021-02-11 18:56:06 +0300

    complete

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # python_text_to_voice
+
+
+install gTTS
+install playsound
+install pyObjC
--- speach_assistant/main.py
+++ speach_assistant/main.py
@@ -1,28 +1,61 @@
 import speech_recognition as srec
+from time import ctime
+import time
+import playsound
+import os
+import random #for generating files for the ausio file
+from gtts import gTTS
+
 
 r = srec.Recognizer()
 
-def recognize_speech():
+def recognize_speech(ask=False):
     with srec.Microphone() as source:
+        if ask:
+            rai_speak(ask)
         audio = r.listen(source)
         voice_data = ''
         
         try:
             voice_data = r.recognize_google(audio)
         except srec.UnknownValueError:
-            print("Sorry! I didn't get you")
+            rai_speak("Sorry! I didn't get you")
         except srec.RequestError:
-            print("Sorry am currently down")
+            rai_speak("Sorry am currently down")
         return voice_data
+    
+def rai_speak():
+    text_to_speach = gTTS(audio_string, lang='en')
+    r = random.randint(1, 10000000)
+    audio_file = 'audio-' + str(r) +'.mp3'
+    text_to_speach.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
 
 def voice_response(voice_data):
     if 'what is your name' in voice_data:
-        print('My name if retech voice assistant')
+        rai_speak('My name RAI, and Mr Gift is my Creator')
+    if "RAI what time is it" in voice_data:
+        rai_speak(ctime())
+    if "RAI search" in voice_data:
+        search = recognize_speech("Hello, What do you want to search?")
+        url='https://google.com/search?q='+search
+        webbrowser.get().open(url)
+        rai_speak('Here is what I found for'+search)
+    if "find location" in voice_data:
+        location = recognize_speech("What is the location")
+        url = 'https://google.nl/maps/place/'+location+'/&amp;'
+        rai_speak('Here is the location')
+        
+    if "power off" in voice_data:
+        rai_speak("Goodbye")
+        exit()
     
-    
-print("Hello!.. How can I help you")
-voice_data = recognize_speech()
-print(voice_data)
+time.sleep(1)
+rai_speak("Hello!.. How can I help you")
 
-voice_response(voice_data)
+while 1:
+    voice_data = recognize_speech()
+    voice_response(voice_data)
     
